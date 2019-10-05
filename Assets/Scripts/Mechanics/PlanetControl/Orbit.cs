77cf4e02362bf7a976287b3a88efb1ba98fcde8a
VoicelessGamer <a.matthews42@outlook.com>
2019-10-05 20:38:35 +0100

    Orbiting updated to work based on local positioning. Orbit script now works for moons.

--- Assets/Scripts/Mechanics/PlanetControl/Orbit.cs
+++ Assets/Scripts/Mechanics/PlanetControl/Orbit.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Orbit : MonoBehaviour {
 
-    public GameObject orbitingObject;
     public float minFociRadius;
     public float maxFociRadius;
     public float minPlanetSeparation;
@@ -18,26 +17,21 @@ public class Orbit : MonoBehaviour {
     private Vector3 foci2;
     private Vector3 centre;
     private float ellipseRotation;
-
-    //TESTING
-    public GameObject testObject;
-    public GameObject testObject2;
-    public GameObject testObject3;
+    private Vector3 localCentreVector;
     private float currentTheta;
 
-    // Start is called before the first frame update
-    void Start() {
+    public void setupOrbit() {
 
         float fociRadius = Random.Range(minFociRadius, maxFociRadius);
         float fociRotation = Random.Range(0, 360);
 
         //first foci point is the centre of the orbiting object
         //second foci point is randomly chosen distance and rotation from the first
-        foci1 = orbitingObject.transform.position;
-        foci2 = new Vector3(fociRadius * Mathf.Sin(fociRotation), fociRadius * Mathf.Cos(fociRotation), 0);
+        foci1 = gameObject.transform.parent.position;
+        foci2 = new Vector3(foci1.x + (fociRadius * Mathf.Sin(fociRotation)), foci1.y + (fociRadius * Mathf.Cos(fociRotation)), 0);
 
         //radius of both planetary object added together with the minimum separation between the 2 planetary objects
-        float minDistanceFromFoci = ((CircleCollider2D)orbitingObject.GetComponent(typeof(CircleCollider2D))).radius +
+        float minDistanceFromFoci = ((CircleCollider2D)gameObject.transform.parent.GetComponent(typeof(CircleCollider2D))).radius +
             ((CircleCollider2D)gameObject.GetComponent(typeof(CircleCollider2D))).radius + minPlanetSeparation;
 
         //minimum distance planet centre should be from either foci to ensure no collision with orbiting object
@@ -47,7 +41,10 @@ public class Orbit : MonoBehaviour {
         float fociDistance = Mathf.Sqrt(Mathf.Pow(foci2.x - foci1.x, 2.0f) + Mathf.Pow(foci2.y - foci1.y, 2.0f));
 
         //centre point of the ellipse
-        centre = new Vector3((foci2.x - foci1.x) / 2, (foci2.y - foci1.y) / 2, 0);
+        centre = new Vector3(foci1.x + ((foci2.x - foci1.x) / 2), foci1.y + ((foci2.y - foci1.y) / 2), 0);
+
+        //Position vector of the foci centre with respect to the body being orbited
+        localCentreVector = new Vector3(foci1.x - centre.x, foci1.y - centre.y, 0);
 
         semiMajorAxis = (fociDistance / 2) + distanceFromFoci;
 
@@ -57,33 +54,23 @@ public class Orbit : MonoBehaviour {
 
         ellipseRotation = Mathf.Atan((foci2.y - foci1.y) / (foci2.x - foci1.x));
 
-        //Testing        
-        GameObject go = (GameObject)Instantiate(testObject2, foci1, Quaternion.identity);
-        go = (GameObject)Instantiate(testObject2, foci2, Quaternion.identity);
-        go = (GameObject)Instantiate(testObject3, centre, Quaternion.identity);
+        currentTheta = Random.Range(0, 360);
 
-        for(int i = 0; i < 360; i++) {
-            go = (GameObject)Instantiate(testObject, getPositionInOrbit(i), Quaternion.identity);
-        }
-
-        currentTheta = 0;
-
-        gameObject.transform.position = getPositionInOrbit(currentTheta);
+        gameObject.transform.localPosition = getPositionInOrbit(currentTheta);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         currentTheta += (planetVelocity * Time.deltaTime);
-        gameObject.transform.position = getPositionInOrbit(currentTheta);
-
+        gameObject.transform.localPosition = getPositionInOrbit(currentTheta);
     }
 
     private Vector3 getPositionInOrbit(float theta) {
         theta = theta * Mathf.Deg2Rad;
         
-        return new Vector3((semiMajorAxis * Mathf.Cos(theta) * Mathf.Cos(ellipseRotation)) - (semiMinorAxis * Mathf.Sin(theta) * Mathf.Sin(ellipseRotation)) + centre.x,
-            (semiMajorAxis * Mathf.Cos(theta) * Mathf.Sin(ellipseRotation)) + (semiMinorAxis * Mathf.Sin(theta) * Mathf.Cos(ellipseRotation)) + centre.y,
+        return new Vector3((semiMajorAxis * Mathf.Cos(theta) * Mathf.Cos(ellipseRotation)) - (semiMinorAxis * Mathf.Sin(theta) * Mathf.Sin(ellipseRotation)) + localCentreVector.x,
+            (semiMajorAxis * Mathf.Cos(theta) * Mathf.Sin(ellipseRotation)) + (semiMinorAxis * Mathf.Sin(theta) * Mathf.Cos(ellipseRotation)) + localCentreVector.y,
             0);
     }
 }
---
+++ Assets/Scripts/Mechanics/PlanetControl/StarSystemGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarSystemGenerator : MonoBehaviour
+{
+    public GameObject systemCentreMass;
+    public GameObject planetPrefab;
+    public GameObject moonPrefab;
+    public int totalPlanets;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+        GameObject centreMass = (GameObject)Instantiate(systemCentreMass, new Vector3(0,0,0), Quaternion.identity);
+
+        for(int i = 0; i < this.totalPlanets; i++) {
+            GameObject planet = (GameObject)Instantiate(planetPrefab, centreMass.transform);
+            Orbit planetOrbit = ((Orbit)planet.GetComponent(typeof(Orbit)));
+            planetOrbit.setupOrbit();
+            
+            GameObject moon = (GameObject)Instantiate(moonPrefab, planet.transform);
+            Orbit moonOrbit = ((Orbit)moon.GetComponent(typeof(Orbit)));
+            moonOrbit.setupOrbit();
+        }
+    }
+}
