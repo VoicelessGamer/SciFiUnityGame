2bb799f47334cb44b4636e29fa7e7ece7039396f
VoicelessGamer <a.matthews42@outlook.com>
2019-10-06 22:11:18 +0100

    Simple scene change from star system screen to planet scene when ship is over a planet or moon and the X key is pressed

---
+++ Assets/Scripts/Mechanics/PlanetLandControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlanetLandControl : MonoBehaviour {
+
+    private bool sceneChangeAvailable = false;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            sceneChangeAvailable = true;
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.CompareTag("Player")) {
+            sceneChangeAvailable = false;
+        }
+    }
+
+    void Update() {
+        if(sceneChangeAvailable && Input.GetKeyDown(KeyCode.X)) {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
