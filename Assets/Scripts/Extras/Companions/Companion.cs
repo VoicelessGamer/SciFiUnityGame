a6d77ae00836b2704bbffd1c609a225215536f51
VoicelessGamer <a.matthews42@outlook.com>
2019-10-09 22:10:21 +0100

    Base floating companion movement, always following behind player

---
+++ Assets/Scripts/Extras/Companions/Companion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Companion : MonoBehaviour {
+    public GameObject player;
+    protected Transform playerTransform;
+    protected Vector3 velocity = Vector3.zero;
+
+    //change to not be public after testing (place in model file)
+    public float maxSpeed;
+
+    void Awake() {
+        playerTransform = player.transform;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
---
+++ Assets/Scripts/Extras/Companions/FloatingCompanion.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingCompanion : Companion {
+    public float smoothTime;
+    private SpriteRenderer playerSpriteRenderer;
+
+    private bool plDir;
+
+    // Start is called before the first frame update
+    void Start() {
+        playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate() {
+        plDir = playerSpriteRenderer.flipX;
+
+        Vector3 targetPosition = new Vector3(this.playerTransform.position.x + (plDir == true ? 2 : -2), 
+            playerTransform.position.y + 2, playerTransform.position.z);
+        //Vector3 targetPosition = new Vector3(playerTransform.position.x, playerTransform.position.y + 2, playerTransform.position.z);
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime, maxSpeed);
+    }
+}
