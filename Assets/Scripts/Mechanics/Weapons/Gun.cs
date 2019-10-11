284040bf81670b24d6a1d573870ee717a89f9b87
VoicelessGamer <a.matthews42@outlook.com>
2019-10-12 00:31:38 +0100

    Bullet spawn with correct rotation

---
+++ Assets/Scripts/Mechanics/Weapons/Gun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : Weapon {
+    public int damage;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")) {
+            GameObject bullet = (GameObject)Instantiate(bulletPrefab, transform.position, getInitialRotation());
+        }
+    }
+}
--- Assets/Scripts/Mechanics/Weapons/Weapon.cs
+++ Assets/Scripts/Mechanics/Weapons/Weapon.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorFollow : MonoBehaviour {
+public class Weapon : MonoBehaviour {
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+        
+    }
+
+    protected Quaternion getInitialRotation() {        
         Vector3 mouse = Input.mousePosition;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 offset = new Vector2(mouse.x - screenPoint.x, mouse.y - screenPoint.y);
-        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        return Quaternion.Euler(0, 0, Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg);
     }
 }
