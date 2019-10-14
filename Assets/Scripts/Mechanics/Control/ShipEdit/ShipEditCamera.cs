d595d56fa25a672533c0bda3566286b402401867
VoicelessGamer <a.matthews42@outlook.com>
2019-10-14 22:15:31 +0100

    Made a start on the ship editor scene

---
+++ Assets/Scripts/Mechanics/Control/ShipEdit/ShipEditCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipEditCamera : MonoBehaviour {
+    public float zoomAmount = 0; //With Positive and negative values
+    public float minZoomClamp = 2;
+    public float maxZoomClamp = 8;
+
+    private Camera cam;
+
+    void Awake() {
+        cam = this.GetComponent<Camera>();
+    }
+
+    void Update() {
+        float scrollAxis = Input.GetAxis("Mouse ScrollWheel");
+
+        cam.orthographicSize = Mathf.Clamp(cam.orthographicSize - scrollAxis, minZoomClamp, maxZoomClamp);
+    }
+}
---
+++ Assets/Scripts/Mechanics/Control/ShipEdit/ShipEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipEditor : MonoBehaviour {
+
+    public GameObject testObject;
+    public GameObject selectedObject;
+
+    void Start() {
+        setSelectedObject(testObject);
+    }
+
+    public void setSelectedObject(GameObject go) {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        selectedObject = Instantiate(go, pos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        pos.z = 0;
+        selectedObject.transform.position = pos;
+    }
+}
