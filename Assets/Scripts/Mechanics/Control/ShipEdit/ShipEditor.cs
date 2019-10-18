d9cca8cabbc3a544eed213b6ed7de7d2896dafd7
VoicelessGamer <a.matthews42@outlook.com>
2019-10-18 22:04:57 +0100

    Placing tilemaps on the grid works along with rules to only place when can be placed without overlap of other tilemaps and within the bounds

--- Assets/Scripts/Mechanics/Control/ShipEdit/ShipEditor.cs
+++ Assets/Scripts/Mechanics/Control/ShipEdit/ShipEditor.cs
@@ -8,9 +8,16 @@ public class ShipEditor : MonoBehaviour {
     public GameObject testObject;
     public GameObject selectedObject;
     public GameObject gridObject;
+    public Vector2[] gridPolygonBounds;
 
     private Color activeColour;
     private Tilemap activeTilemap;
+
+    List<Vector3> occupiedSpaces;
+
+    private void Awake() {
+        occupiedSpaces = new List<Vector3>();
+    }
 
     void Start() {
         testObject.GetComponent<Tilemap>().CompressBounds();
@@ -36,13 +43,72 @@ public class ShipEditor : MonoBehaviour {
         //update the position of the object following the mouse
         selectedObject.transform.position = pos;
 
+        //once clicked
         if (Input.GetButtonDown("Fire1")) {
-            activeColour.a = 1f;
-            activeTilemap.color = activeColour;
 
-            Tilemap tm = selectedObject.GetComponent<Tilemap>();
-            Debug.Log(tm.cellBounds);
-            setSelectedObject(testObject);
+            bool canPlace = true;
+
+            Tilemap tilemap = selectedObject.GetComponent<Tilemap>();
+
+            List<Vector3> tilemapPositions = new List<Vector3>();
+
+            //check all tiles in the current selected object
+            for (int r = tilemap.cellBounds.xMin; r < tilemap.cellBounds.xMax; r++) {
+                for (int c = tilemap.cellBounds.yMin; c < tilemap.cellBounds.yMax; c++) {
+                    //local position in tilemap
+                    Vector3Int localPos = new Vector3Int(r, c, (int)tilemap.transform.position.z);
+
+                    //check there is a tile in the currect position
+                    if (tilemap.GetTile(localPos) != null) {
+                        //local position translated to world coordinates
+                        Vector3 worldPos = tilemap.CellToWorld(localPos);
+                        //if position already occupied then can't be placed
+                        if (occupiedSpaces.Contains(worldPos) || !IsPointInPolygon(worldPos)) {
+                            canPlace = false;
+                            break;
+                        } else {
+                            //add the world position to a list 
+                            tilemapPositions.Add(worldPos);
+                        }
+                    }
+                }
+                //quick return
+                if(!canPlace) {
+                    break;
+                }
+            }
+
+            //if tilemap can be placed
+            if (canPlace) {
+                //add all positions to occupid spaces for future checks
+                occupiedSpaces.AddRange(tilemapPositions);
+
+                //make current tilemap fully visible
+                activeColour.a = 1f;
+                activeTilemap.color = activeColour;
+
+                //update the object following the cursor 
+                setSelectedObject(testObject);
+            }
         }
+    }
+
+    public bool IsPointInPolygon(Vector2 point) {
+        int polygonLength = gridPolygonBounds.Length, i = 0;
+        bool inside = false;
+        // x, y for tested point.
+        float pointX = point.x, pointY = point.y;
+        // start / end point for the current polygon segment.
+        float startX, startY, endX, endY;
+        Vector2 endPoint = gridPolygonBounds[polygonLength - 1];
+        endX = endPoint.x;
+        endY = endPoint.y;
+        while (i < polygonLength) {
+            startX = endX; startY = endY;
+            endPoint = gridPolygonBounds[i++];
+            endX = endPoint.x; endY = endPoint.y;
+            inside ^= (endY > pointY ^ startY > pointY) && ((pointX - endX) < (pointY - endY) * (startX - endX) / (startY - endY));
+        }
+        return inside;
     }
 }
