dfdf8e53511af0b56683cbd8f644e23eac6a053b
VoicelessGamer <a.matthews42@outlook.com>
2019-10-19 17:48:58 +0100

    Added buttons to swap the editor mode. Struggling to come up with way to get the deletion mode working

--- Assets/Scripts/Mechanics/Control/ShipEdit/ShipEditor.cs
+++ Assets/Scripts/Mechanics/Control/ShipEdit/ShipEditor.cs
@@ -1,96 +1,136 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class ShipEditor : MonoBehaviour {
 
+    public enum EditorMode {
+        Placement,
+        Deletion
+    }
+
     public GameObject testObject;
-    public GameObject selectedObject;
-    public GameObject gridObject;
+    public GameObject grid;
     public Vector2[] gridPolygonBounds;
 
+    private GameObject attatchedObject;
     private Color activeColour;
     private Tilemap activeTilemap;
 
-    List<Vector3> occupiedSpaces;
+    private EditorMode editorMode;
+
+    private List<Vector3> occupiedSpaces;
+
+    private Ray ray;
+    private RaycastHit hit;
 
     private void Awake() {
         occupiedSpaces = new List<Vector3>();
     }
 
     void Start() {
-        testObject.GetComponent<Tilemap>().CompressBounds();
-        setSelectedObject(testObject);
     }
 
-    public void setSelectedObject(GameObject go) {
-        selectedObject = Instantiate(go, Input.mousePosition, Quaternion.identity, gridObject.transform);
-        activeTilemap = selectedObject.GetComponent<Tilemap>();
+    // Update is called once per frame
+    void Update() {
+        if(attatchedObject != null) {
+            //the snap position on the grid
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pos.x = Mathf.Round(pos.x - 0.5f);
+            pos.y = Mathf.Round(pos.y + 0.5f);
+            pos.z = 0;
+
+            //update the position of the object following the mouse
+            attatchedObject.transform.position = pos;
+        }
+
+        //once clicked
+        if (Input.GetButtonDown("Fire1")) {
+            switch(editorMode) {
+                case EditorMode.Placement:
+                    if(attatchedObject == null) {
+                        break;
+                    }
+                    bool canPlace = true;
+
+                    Tilemap tilemap = attatchedObject.GetComponent<Tilemap>();
+
+                    List<Vector3> tilemapPositions = new List<Vector3>();
+
+                    //check all tiles in the current selected object
+                    for (int r = tilemap.cellBounds.xMin; r < tilemap.cellBounds.xMax; r++) {
+                        for (int c = tilemap.cellBounds.yMin; c < tilemap.cellBounds.yMax; c++) {
+                            //local position in tilemap
+                            Vector3Int localPos = new Vector3Int(r, c, (int)tilemap.transform.position.z);
+
+                            //check there is a tile in the currect position
+                            if (tilemap.GetTile(localPos) != null) {
+                                //local position translated to world coordinates
+                                Vector3 worldPos = tilemap.CellToWorld(localPos);
+                                //if position already occupied then can't be placed
+                                if (occupiedSpaces.Contains(worldPos) || !IsPointInPolygon(worldPos)) {
+                                    canPlace = false;
+                                    break;
+                                } else {
+                                    //add the world position to a list 
+                                    tilemapPositions.Add(worldPos);
+                                }
+                            }
+                        }
+                        //quick return
+                        if (!canPlace) {
+                            break;
+                        }
+                    }
+
+                    //if tilemap can be placed
+                    if (canPlace) {
+                        //add all positions to occupid spaces for future checks
+                        occupiedSpaces.AddRange(tilemapPositions);
+
+                        //make current tilemap fully visible
+                        activeColour.a = 1f;
+                        activeTilemap.color = activeColour;
+
+                        //update the object following the cursor 
+                        attatchObject(testObject);
+                    }
+                    break;
+                case EditorMode.Deletion:
+                    Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    cursorPos.z = grid.transform.position.z;
+                    foreach(Transform transform in grid.transform) {
+                        cursorPos.z = transform.position.z;
+                        if(transform.GetComponent<CompositeCollider2D>().bounds.Contains(cursorPos)) {
+                            Debug.Log("HIT");
+                        } else {
+                            Debug.Log("NOPE");
+                        }
+                    }
+                    /*ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out hit)) {
+                        GameObject g = hit.transform.gameObject;
+                        Debug.Log("HIT");
+                    } else {
+                        Debug.Log("NOPE");
+                    }*/
+                    break;
+            }
+        }
+    }
+
+    public void attatchObject(GameObject go) {
+        go.GetComponent<Tilemap>().CompressBounds();
+        attatchedObject = Instantiate(go, Input.mousePosition, Quaternion.identity, grid.transform);
+        activeTilemap = attatchedObject.GetComponent<Tilemap>();
         activeColour = activeTilemap.color;
         activeColour.a = 0.5f;
         activeTilemap.color = activeColour;
     }
 
-    // Update is called once per frame
-    void Update() {
-        //the snap position on the grid
-        Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        pos.x = Mathf.Round(pos.x - 0.5f);
-        pos.y = Mathf.Round(pos.y + 0.5f);
-        pos.z = 0;
-
-        //update the position of the object following the mouse
-        selectedObject.transform.position = pos;
-
-        //once clicked
-        if (Input.GetButtonDown("Fire1")) {
-
-            bool canPlace = true;
-
-            Tilemap tilemap = selectedObject.GetComponent<Tilemap>();
-
-            List<Vector3> tilemapPositions = new List<Vector3>();
-
-            //check all tiles in the current selected object
-            for (int r = tilemap.cellBounds.xMin; r < tilemap.cellBounds.xMax; r++) {
-                for (int c = tilemap.cellBounds.yMin; c < tilemap.cellBounds.yMax; c++) {
-                    //local position in tilemap
-                    Vector3Int localPos = new Vector3Int(r, c, (int)tilemap.transform.position.z);
-
-                    //check there is a tile in the currect position
-                    if (tilemap.GetTile(localPos) != null) {
-                        //local position translated to world coordinates
-                        Vector3 worldPos = tilemap.CellToWorld(localPos);
-                        //if position already occupied then can't be placed
-                        if (occupiedSpaces.Contains(worldPos) || !IsPointInPolygon(worldPos)) {
-                            canPlace = false;
-                            break;
-                        } else {
-                            //add the world position to a list 
-                            tilemapPositions.Add(worldPos);
-                        }
-                    }
-                }
-                //quick return
-                if(!canPlace) {
-                    break;
-                }
-            }
-
-            //if tilemap can be placed
-            if (canPlace) {
-                //add all positions to occupid spaces for future checks
-                occupiedSpaces.AddRange(tilemapPositions);
-
-                //make current tilemap fully visible
-                activeColour.a = 1f;
-                activeTilemap.color = activeColour;
-
-                //update the object following the cursor 
-                setSelectedObject(testObject);
-            }
-        }
+    public void removeAttatchedObject() {
+        Destroy(attatchedObject);
+        attatchedObject = null;
     }
 
     public bool IsPointInPolygon(Vector2 point) {
@@ -110,5 +150,18 @@ public class ShipEditor : MonoBehaviour {
             inside ^= (endY > pointY ^ startY > pointY) && ((pointX - endX) < (pointY - endY) * (startX - endX) / (startY - endY));
         }
         return inside;
+    }
+
+    public void changeEditorMode(int newMode) {
+        switch(newMode) {
+            case 0:
+                editorMode = EditorMode.Placement;
+                attatchObject(testObject);
+                break;
+            case 1:
+                editorMode = EditorMode.Deletion;
+                removeAttatchedObject();
+                break;
+        }
     }
 }
