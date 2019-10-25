8f38c4ffa10146121bd63ea40e633b05d9e52e5d
VoicelessGamer <a.matthews42@outlook.com>
2019-10-25 17:30:08 +0100

    Generating the first icon and line in code

---
+++ Assets/Scripts/Mechanics/Control/StarMap/StarMapManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarMapManager : MonoBehaviour {
+    public GameObject starmapIcon;
+
+    // Start is called before the first frame update
+    void Start() {
+        buildMap();
+    }
+
+    private void buildMap() {
+        GameObject icon = Instantiate(starmapIcon, new Vector3(0,0,0), Quaternion.identity);
+        icon.name = "icon";
+
+        GameObject connection = Instantiate(new GameObject(), new Vector3(0, 0, 0), Quaternion.identity);
+        connection.name = "connection";
+        LineRenderer lineRenderer = connection.AddComponent<LineRenderer>();
+
+        Vector3[] positions = new Vector3[2];
+        positions[0] = new Vector3(0,0,0);
+        positions[1] = new Vector3(10, 10, 0);
+
+        lineRenderer.SetPositions(positions);
+    }
+}
