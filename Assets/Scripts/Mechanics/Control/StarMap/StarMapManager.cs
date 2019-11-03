79b9deadddd0ff2f4b3ec7b5aab85f049ad3da96
VoicelessGamer <a.matthews42@outlook.com>
2019-11-03 22:54:07 +0000

    Bug found. Means can't use this method exactly.

--- Assets/Scripts/Mechanics/Control/StarMap/StarMapManager.cs
+++ Assets/Scripts/Mechanics/Control/StarMap/StarMapManager.cs
@@ -7,87 +7,126 @@ public class StarMapManager : MonoBehaviour {
 
     [Range(0, 3)]
     public int minNewConnections;
-
     [Range(0, 3)]
     public int maxNewConnections;
+    public float safetyAngle;
+    public float minimumConnectionDistance;
+    public float maximumConnectionDistance;
 
+    private float radSafteyAngle;
     private readonly float twoPi = Mathf.PI * 2;
+    private Dictionary<GameObject, ConnectionInformation> systemConnections;
 
-    private Dictionary<GameObject, List<GameObject>> systemConnections;
+    public int TESTERINT = 5;
 
     // Start is called before the first frame update
     void Start() {
-        systemConnections = new Dictionary<GameObject, List<GameObject>>();
+        radSafteyAngle = safetyAngle * Mathf.Deg2Rad;
+        systemConnections = new Dictionary<GameObject, ConnectionInformation>();
         buildMap();
     }
 
     private void buildMap() {
-        generateConnectedSystems(createIcon(new Vector3(0,0,0)));
+        GameObject initialSystem = createIcon(new Vector3(0, 0, 0));
+        systemConnections.Add(initialSystem, new ConnectionInformation(null, new List<GameObject>()));
+        generateConnectedSystems(initialSystem);
+
+        int index = 1;
+        while(TESTERINT > 0) {
+            TESTERINT -= 1;
+
+            GameObject[] keys = new GameObject[systemConnections.Count];
+            systemConnections.Keys.CopyTo(keys, 0);
+
+            for (int i = index; i < keys.Length; i++) {
+                generateConnectedSystems(keys[i]);
+            }
+
+            index = keys.Length;
+        }
     }
 
-    private void generateConnectedSystems(GameObject originObject) {
-        Vector3 originPoint = originObject.transform.position;
-        int connectionsToCreate = Random.Range(minNewConnections, maxNewConnections);
+    private void generateConnectedSystems(GameObject system) {
+        //calculate direction offset this system is from its origin point
+        GameObject originObject = systemConnections[system].originSystem;
+        Vector3 distanceFromOrigin = originObject != null ? system.transform.position - originObject.transform.position : new Vector3();
+        float originDirectionOffset = originObject != null ? Mathf.Atan2(distanceFromOrigin.y, distanceFromOrigin.x) : 0;
+
+        Vector3 systemPoint = system.transform.position;
+
+        float closestNegativeAngle = 0, closestPositiveAngle = 0;
+        float[] generationZone = new float[2];
+
+        int connectionsToCreate = Random.Range(minNewConnections, maxNewConnections + 1);
 
         List<GameObject> connectedSystems = new List<GameObject>();
 
-        GameObject TESTOBJ = null;
-
         for (int i = 0; i < connectionsToCreate; i++) {
-            float radAngle = Random.Range(0, twoPi);
+            //find the minimum and maximum angles to generate a new system between
+            generationZone = calculateGenerationZone(system);
+            closestNegativeAngle = generationZone[0] + radSafteyAngle;
+            closestPositiveAngle = generationZone[1] - radSafteyAngle;
+            
+            //get new angle
+            float radAngle = originDirectionOffset + Random.Range(closestNegativeAngle, closestPositiveAngle);
             Vector3 relativeDir = new Vector3(Mathf.Cos(radAngle), Mathf.Sin(radAngle), 0);
-            Vector3 newPosition = relativeDir * Random.Range(5, 30);
+            Vector3 newPosition = systemPoint + (relativeDir * Random.Range(minimumConnectionDistance, maximumConnectionDistance));
 
+            //create new system in caculated position
             GameObject icon = createIcon(newPosition);
             connectedSystems.Add(icon);
 
             Vector3[] positions = new Vector3[2];
-            positions[0] = originPoint;
+            positions[0] = systemPoint;
             positions[1] = newPosition;
 
+            //create connection line
             createConnection(positions);
 
-            //***Testing***
-            if(i == connectionsToCreate - 1) {
-                TESTOBJ = icon;
-            }
-            //******
-
-            systemConnections.Add(icon, new List<GameObject>() { originObject });
+            systemConnections.Add(icon, new ConnectionInformation(system, new List<GameObject>()));
         }
 
-        systemConnections.Add(originObject, connectedSystems);
+        systemConnections[system].connectedSystems = connectedSystems;
+    }
 
-        //***Testing***
-        Vector3 relDir = TESTOBJ.transform.position - originPoint;
-        //Debug.Log("rel.dir: " + relDir); 
-        float rot1 = Mathf.Atan2(relDir.y, relDir.x);
-        Debug.Log("rel rot to origin: " + (rot1 * Mathf.Rad2Deg));
+    private float[] calculateGenerationZone(GameObject system) {
+        GameObject originObject = systemConnections[system].originSystem;
 
-        //Debug.Log("this: " + TESTOBJ.transform.position);
+        float closestNegativeAngle = -Mathf.PI;
+        float closestPositiveAngle = Mathf.PI;
 
-        foreach (GameObject key in systemConnections.Keys) {
-            if(TESTOBJ != key) {
-                Debug.Log("other: " + key.transform.position);
+        if (originObject != null) {
+            Vector3 originPoint = originObject.transform.position;
 
-                //float angle = Vector3.SignedAngle(key.transform.position, relDir, Vector3.back);
-               
-                Vector3 currentDirection = key.transform.position - TESTOBJ.transform.position;
-                float rot2 = Mathf.Atan2(currentDirection.y, currentDirection.x);
-                Debug.Log("rot2: " + (rot2 * Mathf.Rad2Deg));
-                float rot3 = rot2 - rot1;
-                Debug.Log("rot3 before: " + (rot3 * Mathf.Rad2Deg));
-                if(rot3 < -Mathf.PI) {
-                    rot3 += twoPi;
-                } else if(rot3 > Mathf.PI) {
-                    rot3 -= twoPi;
+            //Get direction from this system' origin point
+            Vector3 distanceFromOrigin = system.transform.position - originPoint;
+            float originDirectionOffset = Mathf.Atan2(distanceFromOrigin.y, distanceFromOrigin.x);
+
+            //check against all existsing systems
+            foreach (GameObject key in systemConnections.Keys) {
+                if (system != key) {
+                    //relative distance the current iterated system is from this system
+                    Vector3 relativeDistance = key.transform.position - system.transform.position;
+                    float relativeAngleToSystem = Mathf.Atan2(relativeDistance.y, relativeDistance.x) - originDirectionOffset;
+
+                    //make sure the relative angle is between -pi and +pi
+                    if (relativeAngleToSystem < -Mathf.PI) {
+                        relativeAngleToSystem += twoPi;
+                    } else if (relativeAngleToSystem > Mathf.PI) {
+                        relativeAngleToSystem -= twoPi;
+                    }
+
+                    //update the closest positive and negative angles to 0
+                    if (relativeAngleToSystem < 0 && relativeAngleToSystem > closestNegativeAngle) {
+                        closestNegativeAngle = relativeAngleToSystem;
+                    } else if (relativeAngleToSystem > 0 && relativeAngleToSystem < closestPositiveAngle) {
+                        closestPositiveAngle = relativeAngleToSystem;
+                    }
                 }
-                Debug.Log("rot3 after: " + (rot3 * Mathf.Rad2Deg));
-                /*float angle = -(rot - Mathf.PI);
-                Debug.Log("angle: " + angle);*/
             }
         }
-        //******
+
+        return new float[2] { closestNegativeAngle, closestPositiveAngle };
     }
 
     private GameObject createIcon(Vector3 position) {
@@ -105,5 +144,17 @@ public class StarMapManager : MonoBehaviour {
         lineRenderer.SetPositions(positions);
 
         return connection;
+    }
+
+    protected class ConnectionInformation {
+
+        public GameObject originSystem;
+
+        public List<GameObject> connectedSystems;
+
+        public ConnectionInformation(GameObject originSystem, List<GameObject> connectedSystems) {
+            this.originSystem = originSystem;
+            this.connectedSystems = connectedSystems;
+        }
     }
 }
