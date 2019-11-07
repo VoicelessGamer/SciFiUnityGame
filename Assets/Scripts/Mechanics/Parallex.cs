10853174261889d86c2a83cdc99a92f023d255f3
David Norwood <djnbud@hotmail.co.uk>
2019-11-07 21:17:13 +0000

    Updated star system background

--- Assets/Scripts/Mechanics/Parallex.cs
+++ Assets/Scripts/Mechanics/Parallex.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Parallex : MonoBehaviour
 {
-    private float length, startpos;
+    private float length, startpos, height, startposY;
     public GameObject cam;
     public float parallaxEffect;
+    public float parallaxEffectY;
     // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
+        startpos = cam.transform.position.x;
+        startposY = cam.transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
     }
 
     // Update is called once per frame
@@ -20,11 +23,19 @@ public class Parallex : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        float tempY = (cam.transform.position.y * (1 - parallaxEffectY));
+        float distY = (cam.transform.position.y * parallaxEffectY);
+
+        transform.position = new Vector3(startpos + dist, startposY + distY, transform.position.z);
 
         if (temp > startpos + length)
             startpos += length;
         else if (temp < startpos - length)
             startpos -= length;
+
+        if (tempY > startposY + height)
+            startposY += height;
+        else if (tempY < startposY - height)
+            startposY -= height;
     }
 }
